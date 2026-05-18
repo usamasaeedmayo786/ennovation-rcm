c3292ce8bd5228eea5b7e0c37288a84c31b4f9da
E-nnovationrcm Admin <admin@ennovationrcm.com>
2026-05-18 12:02:54 -0700

    Hide Comparisons option site-wide and add redirect rule to vercel.json

--- bust_cache.ps1
+++ bust_cache.ps1
@@ -1,6 +1,6 @@
 $files = Get-ChildItem -Path . -Recurse -Filter "*.html"
 foreach ($file in $files) {
     $content = Get-Content $file.FullName
-    $content = $content -replace 'href="assets/custom-enhancements.css\?v=4"', 'href="assets/custom-enhancements.css?v=5"'
+    $content = $content -replace 'href="assets/custom-enhancements.css\?v=5"', 'href="assets/custom-enhancements.css?v=6"'
     Set-Content -Path $file.FullName -Value $content
 }
